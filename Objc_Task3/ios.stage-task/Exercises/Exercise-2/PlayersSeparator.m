99e01585b401fb7057a6a2c888c859f6390332b7
alex-pavlovecccc <alex.pavloveccccc@gmail.com>
2021-05-24 01:27:39 +0300

    feat: edit task solution

--- Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
@@ -4,23 +4,36 @@
 - (NSInteger)dividePlayersIntoTeams:(NSArray<NSNumber *>*)ratingArray {
 
     
-    NSInteger count = 0;
+    NSMutableArray *results = [[NSMutableArray alloc] init];
+    NSMutableArray *countArray = [[NSMutableArray alloc] init];
     
-    for (int i = 0 ; i < ratingArray.count; i ++) {
-        for( int j = i + 1; j< ratingArray.count; j ++) {
-            for( int k = j + 1; ratingArray.count; k++){
-                
-                if((ratingArray[i].intValue < ratingArray[j].intValue ||
-                    ratingArray[j].intValue < ratingArray[k].intValue) &&
-                   (ratingArray[i].intValue > ratingArray[j].intValue ||
-                    ratingArray[j].intValue > ratingArray[k].intValue)) {
-                    
-                    count ++;
+    
+    for (int i = 0; i < ratingArray.count; i++) {
+        for (int j = i + 1; j < ratingArray.count; j++) {
+            for (int k = j + 1; k < ratingArray.count; k++) {
+                if (i < j && j < k) {
+                    if (((ratingArray[i].intValue < ratingArray[j].intValue) &&
+                         (ratingArray[j].intValue < ratingArray[k].intValue)) ||
+                        ((ratingArray[i].intValue > ratingArray[j].intValue) &&
+                         (ratingArray[j].intValue > ratingArray[k].intValue))) {
+                        
+                        [countArray addObject:ratingArray[i]];
+                        [countArray addObject:ratingArray[j]];
+                        [countArray addObject:ratingArray[k]];
+                        
+                        [results addObject:countArray];
+                        
+                    }
                 }
             }
         }
     }
-    return count;
+    
+    return results.count;
 }
 
 @end
+
+/*
+ 
+ */
