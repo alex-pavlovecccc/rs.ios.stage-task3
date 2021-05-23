78e3c72413b8e6378902dd80afad37692db201de
alex-pavlovecccc <alex.pavloveccccc@gmail.com>
2021-05-23 15:53:16 +0300

    feat: add solution task 1/task 2

--- Objc_Task3/ios.stage-task/Exercises/Exercise-1/DateHelper.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-1/DateHelper.m
@@ -5,25 +5,36 @@
 #pragma mark - First
 
 -(NSString *)monthNameBy:(NSUInteger)monthNumber {
-    return nil;
+        
+    NSArray *values= @[@"January",@"February",@"March",@"April",@"May",@"June",@"July",@"August",@"September",@"October",@"November"];
+    
+    if (monthNumber < 1 || monthNumber > 12) {
+        return nil;
+    }
+    return [values objectAtIndex: monthNumber - 1];
 }
-
 #pragma mark - Second
 
 - (long)dayFromDate:(NSString *)date {
-    return 0;
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+        dateFormatter.dateFormat = @"yyyy-MM-dd'T'HH:mm:ssZZZZZ";
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    NSInteger day = [calendar component:NSCalendarUnitDay fromDate:[dateFormatter dateFromString:date]];
+    return day;
 }
 
 #pragma mark - Third
 
 - (NSString *)getDayName:(NSDate*) date {
+
     return nil;
 }
 
 #pragma mark - Fourth
 
 - (BOOL)isDateInThisWeek:(NSDate *)date {
-    return NO;
+
+    return  NO;
 }
 
 @end
