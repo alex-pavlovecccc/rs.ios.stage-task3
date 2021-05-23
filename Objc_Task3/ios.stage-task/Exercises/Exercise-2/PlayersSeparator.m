51f373fb46ca94a5ece83a2f397b02800aad2bbb
alex-pavlovecccc <alex.pavloveccccc@gmail.com>
2021-05-23 19:10:18 +0300

    feat: add not a complete solution

--- Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
@@ -1,9 +1,26 @@
 #import "PlayersSeparator.h"
 
 @implementation PlayersSeparator
-
 - (NSInteger)dividePlayersIntoTeams:(NSArray<NSNumber *>*)ratingArray {
-    return 0;
+    
+    
+    NSInteger count = 0;
+    
+    for (int i = 0 ; i < ratingArray.count; i ++) {
+        for( int j = i; j< ratingArray.count; j ++) {
+            for( int k = j; ratingArray.count; k++){
+                
+                if((ratingArray[i].intValue < ratingArray[j].intValue &&
+                    ratingArray[j].intValue < ratingArray[k].intValue) ||
+                   (ratingArray[i].intValue > ratingArray[j].intValue &&
+                    ratingArray[j].intValue > ratingArray[k].intValue)) {
+                    
+                    count ++;
+                }
+            }
+        }
+    }
+    return count;
 }
 
 @end
