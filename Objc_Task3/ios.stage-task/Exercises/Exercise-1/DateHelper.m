2665077b13cce61e3ab28947700000f5fb5f5a64
alex-pavlovecccc <alex.pavloveccccc@gmail.com>
2021-05-24 01:29:43 +0300

    feat: edit task solution

--- Objc_Task3/ios.stage-task/Exercises/Exercise-1/DateHelper.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-1/DateHelper.m
@@ -9,7 +9,9 @@
     NSArray *values= @[@"January",@"February",@"March",@"April",@"May",@"June",@"July",@"August",@"September",@"October",@"November"];
     
     if (monthNumber < 1 || monthNumber > 12) {
+        
         return nil;
+        
     }
     
     return [values objectAtIndex: monthNumber - 1];
@@ -22,6 +24,7 @@
     NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
         dateFormatter.dateFormat = @"yyyy-MM-dd'T'HH:mm:ssZZZZZ";
     NSCalendar *calendar = [NSCalendar currentCalendar];
+    
     NSInteger day = [calendar component:NSCalendarUnitDay fromDate:[dateFormatter dateFromString:date]];
     return day;
     
@@ -35,7 +38,9 @@
        NSLocale *locale = [[NSLocale alloc] initWithLocaleIdentifier:@"ru"];
        [dateFormatter setLocale:locale];
        [dateFormatter setDateFormat:@"EE"];
+    
        return [dateFormatter stringFromDate:date];
+    
 }
 
 #pragma mark - Fourth
